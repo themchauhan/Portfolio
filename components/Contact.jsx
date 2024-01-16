8b1347a6627e0dca35cbe3177216d4ce8ca09dc7
Manish <mani7015066@gmail.com>
2024-01-17 01:59:28 +0530

    form working

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,6 +1,6 @@
 // components/ContactForm.js
 "use client"
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Image from 'next/image';
 
 const Contact = () => {
@@ -10,6 +10,7 @@ const Contact = () => {
     subject: "",
     message: "",
   });
+  const formRef = useRef(null);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -19,128 +20,144 @@ const Contact = () => {
     });
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    // Send the form data to your server or API endpoint here
-    // You can use libraries like axios to make HTTP requests
+  async function handleSubmit(event) {
+    event.preventDefault();
+
     try {
-      const response = await fetch("/api/contact", {
-        method: "POST",
+      const response = await fetch('/api/contact', {
+        method: 'post',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(formData),
       });
 
-      if (response.ok) {
-        alert("Form submitted successfully!");
-      } else {
-        alert("Form submission failed.");
+      if (!response.ok) {
+        console.log("falling over");
+        throw new Error(`response status: ${response.status}`);
       }
-    } catch (error) {
-      console.error("Error:", error);
+
+      const responseData = await response.json();
+      console.log(responseData['message']);
+
+      const successMessage = document.getElementById("success-message");
+      successMessage.style.display = "block";
+  
+      // Hide success message after 3 seconds
+      setTimeout(() => {
+        successMessage.style.display = "none";
+      }, 3000);
+    } catch (err) {
+      console.error(err);
+      alert("Error, please try resubmitting the form");
     }
   };
+  
+
 
   return (
     <section className="form_wrap" id="contact">
-      <div className="container">  
-       <div className="title-wrap mb-7">
-         <h3 className="mb-3 font-bold">GET IN TOUCH</h3>
-         <p>If you are a business seeking a web presence, you can get in touch with me here.</p>
-       </div>
-      <div className="form-wrap md:flex"> 
-        <div className="flex w-full md:w-7/12">
-         <form onSubmit={handleSubmit} className="w-full">
-            <div className="mb-4">
+      <div className="container">
+        <div className="title-wrap mb-7">
+          <h3 className="mb-3 font-bold">GET IN TOUCH</h3>
+          <p>If you are a business seeking a web presence, you can get in touch with me here.</p>
+        </div>
+        <div className="form-wrap md:flex">
+          <div className="flex w-full md:w-7/12">
+            <form ref={formRef} onSubmit={handleSubmit} className="w-full">
+              <div className="mb-4">
                 <label htmlFor="name">Name</label>
                 <input
-                type="text"
-                id="name"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                className="w-full p-2 border text-black"
-                required
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  className="w-full p-2 border text-black"
+                  required
                 />
-            </div>
-            <div className="mb-4">
+              </div>
+              <div className="mb-4">
                 <label htmlFor="email">Email</label>
                 <input
-                type="email"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                className="w-full p-2 border text-black"
-                required
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  className="w-full p-2 border text-black"
+                  required
                 />
-            </div>
-            <div className="mb-4">
+              </div>
+              <div className="mb-4">
                 <label htmlFor="subject">Subject</label>
                 <input
-                type="text"
-                id="subject"
-                name="subject"
-                value={formData.subject}
-                onChange={handleChange}
-                className="w-full p-2 border text-black"
-                required
+                  type="text"
+                  id="subject"
+                  name="subject"
+                  value={formData.subject}
+                  onChange={handleChange}
+                  className="w-full p-2 border text-black"
+                  required
                 />
-            </div>
-            <div className="mb-4">
+              </div>
+              <div className="mb-4">
                 <label htmlFor="message">Message</label>
                 <textarea
-                id="message"
-                name="message"
-                value={formData.message}
-                onChange={handleChange}
-                rows="4"
-                className="w-full p-2 border text-black "
-                required
+                  id="message"
+                  name="message"
+                  value={formData.message}
+                  onChange={handleChange}
+                  rows="4"
+                  className="w-full p-2 border text-black "
+                  required
                 />
-            </div>
-            <button type="submit"
+              </div>
+              <div id="success-message" className="success-message" style={{ display: "none" }}>
+                Thank you for submitting form, We will reach you soon!!
+              </div>
+              <button
+                type="submit"
                 className="bg-[#5869a5] text-white px-4 py-2 rounded-full hover:bg-white hover:text-black min-w-[220px] place-self-center place-items-center"
-            >
+              >
                 Submit
-            </button>
-         </form> 
-        </div> 
-        <div className="info-wrap flex flex-wrap w-full md:w-5/12 place-content-start md:pl-20 pt-5">
-           <div className="flex contact-info w-full">
-            <span>
-              <Image 
-                 src="/phone-call.svg"
-                 alt="person"
-                 width={24}
-                 height={24}
-                 style={{ objectFit: 'contain'}} 
-               />
-            </span>
-            <span className="pl-4">
+              </button>
+            </form>
+          </div>
+          <div className="info-wrap flex flex-wrap w-full md:w-5/12 place-content-start md:pl-20 pt-5">
+            <div className="flex contact-info w-full">
+              <span>
+                <Image
+                  src="/phone-call.svg"
+                  alt="person"
+                  width={24}
+                  height={24}
+                  style={{ objectFit: 'contain' }}
+                />
+              </span>
+              <span className="pl-4">
                 <h6 className="text-[#f04e3c] text-base">+917015066237</h6>
                 <h6 className="mt-3 text-base">Mon to Fri (10am - 6pm)</h6>
-            </span>
-           </div>
-           <div className="flex contact-info w-full pt-6">
-            <span>
-              <Image 
-                 src="/email.svg"
-                 alt="person"
-                 width={24}
-                 height={24}
-                 style={{ objectFit: 'contain'}} 
-               />
-            </span>
-            <span className="pl-4">
+              </span>
+            </div>
+            <div className="flex contact-info w-full pt-6">
+              <span>
+                <Image
+                  src="/email.svg"
+                  alt="person"
+                  width={24}
+                  height={24}
+                  style={{ objectFit: 'contain' }}
+                />
+              </span>
+              <span className="pl-4">
                 <h6 className="text-base"><a href="mailto:mani7015066@gmail.com" className="underline text-[#f04e3c] hover:no-underline">Let&#39;s Connect in your Inbox</a></h6>
                 <h6 className="mt-3 text-base">Feel Free To Reach Out Anytime!</h6>
-            </span>
-           </div>
+              </span>
+            </div>
+          </div>
         </div>
-        </div>
-      </div>   
+      </div>
     </section>
   );
 };
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,51 +1,49 @@
-// pages/api/contact.js
+import { mailOptions, transporter } from "../../config/nodemailer";
 
-export default async function handler(req, res) {
-    if (req.method === "POST") {
-      const formData = req.body;
-  
-      // Here, you can implement code to send the form data to your email
-      // For example, you can use a nodemailer library to send an email
-  
-      try {
-        // Implement email sending logic here
-        // Example using nodemailer:
-        /*
-        const nodemailer = require("nodemailer");
-        const transporter = nodemailer.createTransport({
-          service: "YourEmailService",
-          auth: {
-            user: "YourEmailAddress",
-            pass: "YourEmailPassword",
-          },
-        });
-  
-        const mailOptions = {
-          from: formData.email,
-          to: "YourEmailAddress",
-          subject: formData.subject,
-          text: formData.message,
-        };
-  
-        transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-            console.error("Email error:", error);
-            res.status(500).json({ message: "Email sending failed" });
-          } else {
-            console.log("Email sent:", info.response);
-            res.status(200).json({ message: "Email sent successfully" });
-          }
-        });
-        */
-  
-        // This is a placeholder response
-        res.status(200).json({ message: "Form data received" });
-      } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ message: "Form submission failed" });
-      }
-    } else {
-      res.status(405).json({ message: "Method not allowed" });
+const CONTACT_MESSAGE_FIELDS = {
+  name: "Name",
+  email: "Email",
+  subject: "Subject",
+  message: "Message",
+};
+
+const generateEmailContent = (data) => {
+  const stringData = Object.entries(data).reduce(
+    (str, [key, val]) =>
+      (str += `${CONTACT_MESSAGE_FIELDS[key]}: \n${val} \n \n`),
+    ""
+  );
+  const htmlData = Object.entries(data).reduce((str, [key, val]) => {
+    return (str += `<h3 class="form-heading" align="left">${CONTACT_MESSAGE_FIELDS[key]}</h3><p class="form-answer" align="left">${val}</p>`);
+  }, "");
+
+  return {
+    text: stringData,
+    html: `<!DOCTYPE html><html> <head> <title></title> <meta charset="utf-8"/> <meta name="viewport" content="width=device-width, initial-scale=1"/> <meta http-equiv="X-UA-Compatible" content="IE=edge"/> <style type="text/css"> body, table, td, a{-webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%;}table{border-collapse: collapse !important;}body{height: 100% !important; margin: 0 !important; padding: 0 !important; width: 100% !important;}@media screen and (max-width: 525px){.wrapper{width: 100% !important; max-width: 100% !important;}.responsive-table{width: 100% !important;}.padding{padding: 10px 5% 15px 5% !important;}.section-padding{padding: 0 15px 50px 15px !important;}}.form-container{margin-bottom: 24px; padding: 20px; border: 1px dashed #ccc;}.form-heading{color: #2a2a2a; font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif; font-weight: 400; text-align: left; line-height: 20px; font-size: 18px; margin: 0 0 8px; padding: 0;}.form-answer{color: #2a2a2a; font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif; font-weight: 300; text-align: left; line-height: 20px; font-size: 16px; margin: 0 0 24px; padding: 0;}div[style*="margin: 16px 0;"]{margin: 0 !important;}</style> </head> <body style="margin: 0 !important; padding: 0 !important; background: #fff"> <div style=" display: none; font-size: 1px; color: #fefefe; line-height: 1px;  max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; " ></div><table border="0" cellpadding="0" cellspacing="0" width="100%"> <tr> <td bgcolor="#ffffff" align="center" style="padding: 10px 15px 30px 15px" class="section-padding" > <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 500px" class="responsive-table" > <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0"> <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0" > <tr> <td style=" padding: 0 0 0 0; font-size: 16px; line-height: 25px; color: #232323; " class="padding message-content" > <h2>New Contact Message</h2> <div class="form-container">${htmlData}</div></td></tr></table> </td></tr></table> </td></tr></table> </td></tr></table> </body></html>`,
+  };
+};
+
+const handler = async (req, res) => {
+  if (req.method === "POST") {
+    const data = req.body;
+    if (!data || !data.name || !data.email || !data.subject || !data.message) {
+      return res.status(400).send({ message: "Bad request" });
+    }
+
+    try {
+      await transporter.sendMail({
+        ...mailOptions,
+        ...generateEmailContent(data),
+        subject: data.subject,
+      });
+
+      return res.status(200).json({ success: true });
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json({ message: err.message });
     }
   }
-  
+  return res.status(400).json({ message: "Bad request" });
+};
+
+export default handler;
